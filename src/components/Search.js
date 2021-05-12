d5ff94bb0af309b9a5e86667b6aa9630de6f0931
Denis Memiev <denismemiev@gmail.com>
2021-05-12 17:38:19 +0300

    search widget done

--- src/components/Search.js
+++ src/components/Search.js
@@ -8,33 +8,51 @@ const Search = () =>{
     const [term, setTerm] = useState('programming');
     const [results, setResults] = useState([]);
 
-    useEffect(async () =>{
-
-        const SearchWiki = async () => {
-            const { data } = await axios.get('http://en.wikipedia.org/w/api.php',{
-                params:{
-                    action: 'query',
-                    list: 'search',
-                    origin: '*',
-                    format: 'json',
-                    srsearch: term,
-                },
-            });
-            setResults(data.query.search);
+    useEffect(() => {
+        const search = async () => {
+          const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+            params: {
+              action: 'query',
+              list: 'search',
+              origin: '*',
+              format: 'json',
+              srsearch: term,
+            },
+          });
+    
+          setResults(data.query.search);
         };
-        if(term){
-            SearchWiki();
+    
+        if (term && !results.length) {
+          search();
+        } else {
+          const timeoutId = setTimeout(() => {
+            if (term) {
+              search();
+            }
+          }, 1000);
+    
+          return () => {
+            clearTimeout(timeoutId);
+          };
         }
-
-    }, [term]);
+      }, [term]);
 
     const renderedReasults = results.map( (result) => {
         return <div key={result.pageid} className='item'>
+            <div className="right floated content">
+                <a 
+                className="ui button"
+                href={`https://en.wikipedia.org?curid=${result.pageid}`}
+                >
+                    Go
+                </a>
+            </div>
             <div className='content'>
                 {result.title}
             </div>
             <div>
-            {result.snippet}
+            <span dangerouslySetInnerHTML={{ __html:result.snippet}}></span>
             </div>
         </div>
     });
